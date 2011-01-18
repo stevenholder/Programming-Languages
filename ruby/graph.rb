b6f5813e843c330370dc38c9a225e57db69d8f2d
Steven Holder <stevenholder1@gmail.com>
2011-01-18 13:17:52 -0500

    graph project

--- ruby/graph.rb
+++ ruby/graph.rb
@@ -1,11 +1,14 @@
+require 'singleton'
 class Graph
-  attr_accessor :array
+  include Singleton
+  attr_accessor :array, :name
   
-  def initialize
+  def create name
+    @name = name
     @array = []
    end
    
-   def vertices(number)
+   def vertices number
      i = 0
      while i < number
        array.push([])
@@ -13,12 +16,13 @@ class Graph
      end
    end
    
-   def edge(source, destination)
+   def edge source, destination
      array[source].push(destination)
      array[destination].push(source)
    end
    
    def printArray
+     puts "Array: #{name}"
      i = 0
      array.each do |a|
        print i.to_s + " : "
@@ -27,7 +31,7 @@ class Graph
      end
    end
    
-   def printDegree(vertex)
+   def printDegree vertex
      size = array[vertex].size
      puts "The degree of vertext #{vertex} is #{size}"
    end
@@ -44,7 +48,7 @@ class Graph
      puts "The maximum degree is #{max}"
    end
    
-   def edge?(vertex1, vertex2)
+   def edge? vertex1, vertex2
      if array[vertex1].member?(vertex2) && array[vertex2].member?(vertex1)
        puts "(#{vertex1},#{vertex2}) is an edge."
        true
@@ -54,25 +58,67 @@ class Graph
      end
    end
    
+   def inTriangle? vertex1, vertex2
+     common = (array[vertex1] & array[vertex2])
+     if common.any?
+       puts "Vertex #{common} completes a triangle with #{vertex1} and #{vertex2}"
+     else
+       puts "No triangle contains #{vertex1} and #{vertex2}"
+     end
+   end
+   
 end
 
-g = Graph.new()
+def graph name 
+  Graph.instance.create name
+end
 
-g.vertices 5
+def vertices number
+  Graph.instance.vertices number
+end
 
-g.edge 0, 1
-g.edge 1, 2
-g.edge 2, 4
-g.edge 1, 3
-g.edge 2, 3
+def edge vertex1, vertex2
+  Graph.instance.edge vertex1, vertex2
+end
 
-g.printArray
-g.printDegree 2
-g.printArray
-g.printMaxDegree
+def printArray
+  Graph.instance.printArray
+end
 
-g.edge? 3, 4
-g.edge? 2, 3
-         
-   
+def printDegree vertex
+  Graph.instance.printDegree vertex
+end
+
+def printMaxDegree
+  Graph.instance.printMaxDegree
+end
+
+def edge? vertex1, vertex2
+  Graph.instance.edge? vertex1, vertex2
+end
+
+def inTriangle? vertex1, vertex2
+  Graph.instance.inTriangle? vertex1, vertex2
+end
+
+graph 'G'
+vertices 5
+edge 0, 1
+edge 1, 2
+edge 2, 4
+edge 1, 3
+edge 2, 3
+printArray
+printDegree 2
+printArray
+printMaxDegree
+edge? 3,4
+edge? 2,3
+inTriangle? 1, 2
+inTriangle? 0, 4
+
+
+
+
+
   
